2450613d489e5cd34a713d325fdb84f5728aa008
creatImre <imre.torma93@gmail.com>
2017-11-22 16:43:45 +0100

    used correct security context

--- src/main/java/authentication/AuthenticationFilter.java
+++ src/main/java/authentication/AuthenticationFilter.java
@@ -1,7 +1,6 @@
 package authentication;
 
 import java.io.IOException;
-import java.security.Principal;
 
 import javax.annotation.Priority;
 import javax.ejb.EJB;
@@ -10,7 +9,6 @@ import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import entities.User;
@@ -43,33 +41,7 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 		if(user == null) {
 			throw new NotAuthorizedException("Google authentication failed.");
 		}
-		crc.setSecurityContext(new SecurityContext() {
-			@Override
-			public Principal getUserPrincipal() {
-				return new Principal() {
-					@Override
-					public String getName() {
-						return user.getName();
-					}
-				};
-			}
-
-			@Override
-			public boolean isUserInRole(String string) {
-				return true;
-			}
-
-			@Override
-			public boolean isSecure() {
-				return false;
-			}
-
-			@Override
-			public String getAuthenticationScheme() {
-				return googleToken;
-			}
-		});
-
+		crc.setSecurityContext(new UserSecurityContext(user, crc.getUriInfo()));
 	}
 
 }
