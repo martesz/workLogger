b609079e85d7f9ae4df2ad672323019ca3b03dfc
creatImre <imre.torma93@gmail.com>
2017-11-22 16:56:43 +0100

    fixed reportApi missing annotations, bad securityContext

--- src/main/java/rest/ReportApi.java
+++ src/main/java/rest/ReportApi.java
@@ -1,24 +1,29 @@
 package rest;
 
 import javax.ejb.EJB;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import authentication.UserSecurityContext;
+import authentication.Secured;
 import entities.Project;
 import service.ReportService;
+import service.WorkingHourService;
 
 @Path("/report")
 public class ReportApi {
 
-	@Context
-	private UserSecurityContext securityContext;
-
 	@EJB
 	private ReportService reportService;
+	@EJB
+	private WorkingHourService workingHourService;
 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Secured
 	@Path("/{projectName}")
 	public Response getReportByProjectName(@PathParam(value = "projectName") String projectName) {
 		Project project = reportService.getProjectReport(projectName);
