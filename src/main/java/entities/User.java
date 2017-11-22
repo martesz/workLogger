cfbb5bb5c4e99330a66c7b8121bff3147dbea5c2
creatImre <imre.torma93@gmail.com>
2017-11-22 17:11:24 +0100

    changed bad javadoc text

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * Entity implementation class for Entity: Project
+ * Entity implementation class for Entity: User
  *
  */
 @Entity
