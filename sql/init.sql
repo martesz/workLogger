7395c0457ba49d6182287c6c039e92b24ee24043
creatImre <imre.torma93@gmail.com>
2017-11-24 22:24:15 +0100

    updated init.sql with better texts

--- sql/init.sql
+++ sql/init.sql
@@ -9,21 +9,21 @@ VALUES('101812010762749781224', 'Imre Torma', 'ADMIN');
 
 
 INSERT INTO PROJECT(ID, NAME, DESCRIPTION)
-VALUES(1, 'project1', 'descr1');
+VALUES(1, 'Ultimate search', 'Make better search algorithm.');
 INSERT INTO PROJECT(ID, NAME, DESCRIPTION)
-VALUES(2, 'project2', 'descr2');
+VALUES(2, 'Project Alien', 'Trying out innovative functions.');
 INSERT INTO PROJECT(ID, NAME, DESCRIPTION)
-VALUES(3, 'project3', 'descr3');
+VALUES(3, 'C.R.U.D', 'To implement correct CRUD functions to the software.');
 
 
 INSERT INTO ISSUE(ID, NAME, DESCRIPTION, PROJECT_ID)
-VALUES(1, 'issue1', 'descr1', 1);
+VALUES(1, 'Reduce nested cycles', 'Reducing nested FOR cycles.', 1);
 INSERT INTO ISSUE(ID, NAME, DESCRIPTION, PROJECT_ID)
-VALUES(2, 'issue2', 'descr2', 1);
+VALUES(2, 'Newest Android API', 'To find better solutions.', 2);
 INSERT INTO ISSUE(ID, NAME, DESCRIPTION, PROJECT_ID)
-VALUES(3, 'issue3', 'descr3', 2);
+VALUES(3, 'Screen process', 'To identify objects on screens.', 2);
 INSERT INTO ISSUE(ID, NAME, DESCRIPTION, PROJECT_ID)
-VALUES(4, 'issue4', 'descr4', 3);
+VALUES(4, 'Safer read implementation', 'To avoid some known bugs.', 3);
 
 
 INSERT INTO WORKINGHOUR(ID, STARTING, DURATION, USER_GOOGLEID, ISSUE_ID)
